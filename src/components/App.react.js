1dcd7352f8ebef3c0aaafd80e153c725f1afba7d
Mikael Boutin <mikaelboutin.dw@gmail.com>
2016-08-31 20:15:12 -0400

    Just connected react-intl and page's transitions don\'t work in Safari and ios

--- src/components/App.react.js
+++ src/components/App.react.js
@@ -1,20 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { IntlProvider, FormattedMessage } from 'react-intl';
+
+import i18n from 'i18n';
+import messages from 'language';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      locale: 'en',
+    };
+
+    this.handleLocaleChange = this.handleLocaleChange.bind(this);
+  }
+
+  handleLocaleChange() {
+    const language = (this.state.locale === 'en') ? 'fr' : 'en';
+
+    this.setState({
+      locale: language,
+    });
   }
 
   render() {
     const { children } = this.props;
 
+    const intlData = {
+      locale: this.state.locale,
+      messages: i18n[this.state.locale],
+    };
+
     return (
-      <div>
-        <h1>Hello World <small>from a component</small></h1>
-        <Link to="test">Test link</Link>
-        { children }
-      </div>
+      <IntlProvider key="intl" {...intlData}>
+        <div>
+          <div>
+            <button onClick={this.handleLocaleChange}><FormattedMessage {...messages.localeText} /></button>
+          </div>
+          <h1><FormattedMessage {...messages.helloWorld} /> <small><FormattedMessage {...messages.fromAComponent} /></small></h1>
+          <Link to="test">Test link</Link>
+          { children }
+        </div>
+      </IntlProvider>
     );
   }
 }
